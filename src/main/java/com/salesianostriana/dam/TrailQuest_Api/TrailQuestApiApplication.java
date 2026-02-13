4f3a732e9ef26b319c4682da4d82db152c5fe1a0
Gabriel-Perez-Marquez <gabrielperezmarquez18@gmail.com>
2026-02-13 12:41:14 +0100

    Proyecto creado

---
+++ src/main/java/com/salesianostriana/dam/TrailQuest_Api/TrailQuestApiApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.TrailQuest_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailQuestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailQuestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianostriana/dam/TrailQuest_Api/TrailQuestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.TrailQuest_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailQuestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
