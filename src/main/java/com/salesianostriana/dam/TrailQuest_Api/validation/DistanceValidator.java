5bded7232dd51bc3ed34793bceafb3ea72e5733c
Alejandro <alerodarias@gmail.com>
2026-02-18 23:02:50 +0100

    agregadas validations

---
+++ src/main/java/com/salesianostriana/dam/TrailQuest_Api/validation/DistanceValidator.java
@@ -0,0 +1,15 @@
+package com.salesianostriana.dam.TrailQuest_Api.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class DistanceValidator implements ConstraintValidator<ValidDistance, Double> {
+
+    @Override
+    public boolean isValid(Double distance, ConstraintValidatorContext context) {
+        if (distance == null) {
+            return false;
+        }
+        return distance > 0.1;
+    }
+}
