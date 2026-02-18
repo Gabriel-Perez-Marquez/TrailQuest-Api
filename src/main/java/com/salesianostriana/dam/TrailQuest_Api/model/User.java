5272ef250f0c157ac7d63c00829fb63a4c5e8736
Gabriel-Perez-Marquez <gabrielperezmarquez18@gmail.com>
2026-02-18 13:09:07 +0100

    feat: Entidad terminada con validacion de atributos

--- src/main/java/com/salesianostriana/dam/TrailQuest_Api/model/User.java
+++ src/main/java/com/salesianostriana/dam/TrailQuest_Api/model/User.java
@@ -1,7 +1,12 @@
 package com.salesianostriana.dam.TrailQuest_Api.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.validator.constraints.UniqueElements;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -23,12 +28,24 @@ public class User implements UserDetails {
     @GeneratedValue
     private UUID id;
 
+    @NotBlank(message = "El nombre de usuario no puede estar vacío")
+    @Size(min = 4, max = 20, message = "El usuario debe tener entre 4 y 20 caracteres")
+    @Column(unique = true, nullable = false)
     private String username;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 8, message = "La contraseña debe tener al menos 8 caracteres")
     private String password;
+
+    @Email(message = "Debe proporcionar un correo electrónico válido")
+    @NotBlank(message = "El email no puede estar vacío")
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @NotEmpty(message = "El usuario debe tener al menos un rol")
     @ElementCollection(fetch = FetchType.EAGER)
     @Builder.Default
-    private Set<UserRole> roles = new HashSet<>();;
+    private Set<UserRole> roles = new HashSet<>();
 
 
     @Override
