18ce550c57f04aae6ad9bb3365a48d02acecf8e9
Alejandro <rodriguez.arale24@triana.salesianos.edu>
2026-02-23 09:32:51 +0100

    fix: solucionado error menos donde id de Route era de tipo String en vez de long

--- src/main/java/com/salesianostriana/dam/TrailQuest_Api/model/Route.java
+++ src/main/java/com/salesianostriana/dam/TrailQuest_Api/model/Route.java
@@ -18,7 +18,7 @@ public class Route {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private String id;
+    private Long id;
 
     @Column(nullable = false)
     private String title;
